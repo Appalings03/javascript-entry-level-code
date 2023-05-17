e0824a0b0973ae78a7aa88c861568b9d56854242
Martin Binamé <marti>
2023-05-17 08:34:05 +0200

    reorganisation of the code

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 //using the script in html 
 //also work in the index.js if put in a html file
+    //alert('Hello World')
+    //variable declaration
+    let age;
+
+    //variable assignment
+    age = 50;
+    console.log(age)
+
+    let name = 'Pilot'
+    console.log(name)
+
+    let hasExperience = true;
+    console.log(hasExperience)
+
+    // number
+    let examResult = 50.5;
+    console.log(examResult);
+  
+    // change the value of a variable by reassigning
+    examResult = 100;
+    console.log(examResult);
+  
+    // not recommended
+    examResult = 'Some Text';
+    console.log(examResult);
+  
+    // undefined variable
+    let something;
+    console.log(something);
