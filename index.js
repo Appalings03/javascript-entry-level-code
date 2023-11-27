6b30a6a97b6526e9849f1ffe93f013aaa1f7bbf4
Martin Binamé <marti>
2023-11-27 10:23:36 +0100

    commit object function js

--- index.js
+++ index.js
@@ -197,4 +197,34 @@ sellitem(10)
 sellitem(10)
 sellitem(10)
 
+//java object properties
 
+let player = {
+  age: 22,
+  name: 'Martin',
+  isActive: true,
+  outfit:{
+      color: 'blue',
+      size: 'M',
+      cost: '100',
+      isNew: true
+      }
+  };
+
+console.log(player);
+console.log(player.outfit.color);
+player.outfit.size = 'L';
+console.log(player.outfit.size);
+
+/*console.log(player.name)
+console.log(player['name']);
+
+player.isActive = false;
+console.log(player);
+
+player.health = 100;
+console.log(player);
+
+delete player.health;
+console.log(player.health);
+*/
--- script.js
+++ script.js
@@ -1,10 +1,2 @@
 //Write code Here to export in the HTML
 
-let player = {
-            age: 22,
-            name: 'Martin',
-            isActive: true
-            };
-
-console.log(player)
-console.log(player.name)
