f6b1092928ff22b6121ff1d291beed47db114577
Martin Binamé <marti>
2024-03-27 15:53:35 +0100

    MultiDim Array

--- index.js
+++ index.js
@@ -401,3 +401,19 @@ catalog.forEach(function(entry){
 });
 
 console.log(catalog);
+
+// Multidimensionnal Array
+
+//2D Array
+let a2 =[[1, 2, 3],[4, 5, 6, 7]];
+console.log(a2);
+console.log(a2[0][1]);
+a2[1][2] = 100;
+console.log(a2[1][2]);
+
+let terrains = [
+    ['desert', 'desert', 'grass', 'grass'],
+    ['desert', 'grass', 'water', 'grass'],
+    ['grass', 'grass', 'water', 'water'],
+    ['grass', 'grass', 'grass', 'grass']
+];
